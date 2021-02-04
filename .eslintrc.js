91c9a6d8a8c51d8e3e58bd260b5d94fee100f864
Raul Max <raulmax319@gmail.com>
2021-02-04 18:49:21 -0300

    Navbar component added

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
