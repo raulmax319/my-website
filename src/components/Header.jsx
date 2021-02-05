7a5f53dbdf748d76f38fa9ea7f5c1fa3e195e6be
Raul Max <raulmax319@gmail.com>
2021-02-05 14:09:25 -0300

    Fix: header position fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,26 @@ function Header() {
   return (
     <div className="header-wrapper">
       <div className="main-info">
-        <h2>Hi, I&#39;m</h2>
-        <h1>Raul Max, your</h1>
-        <Typed
-          className="typed-text"
-          strings={[
-            'Backend Developer.',
-            'Frontend Developer.',
-            'Coffee lover.',
-          ]}
-          typeSpeed={40}
-          backSpeed={60}
-          loop
-        />
-        <span className="bt1">
-          <a href="#" className="btn-main-offer">
-            Contact me
-          </a>
-        </span>
+        <div className="content">
+          <h2>Hi, I&#39;m</h2>
+          <h1>Raul Max, your</h1>
+          <Typed
+            className="typed-text"
+            strings={[
+              'Backend Developer.',
+              'Frontend Developer.',
+              'Coffee lover.',
+            ]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+          />
+          <span className="bt1">
+            <a href="#" className="btn-main-offer">
+              Contact me
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
