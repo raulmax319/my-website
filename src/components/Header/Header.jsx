e35c1bd1f4b09a26170185b82cbdaaeb2a95af68
Raul Max <raulmax319@gmail.com>
2021-02-11 16:12:38 -0300

    Header style and content finished (ref #3 )

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Fade, Reveal } from 'react-reveal';
 
-// import { Container } from './styles';
+import {
+  Home,
+  BannerArea,
+  Container,
+  BannerContent,
+  SocialLinks,
+} from './styled/Header.styled';
 
-function Header() {
-  return <div />;
+function Header({ name, socialLinks, typedText }) {
+  return (
+    <Home>
+      <BannerArea>
+        <Container>
+          <BannerContent>
+            <Reveal effect="fadeInUp">
+              <h5>HELLO</h5>
+            </Reveal>
+            <Reveal effect="fadeInUp">
+              <h2>I&#39;m Raul Max</h2>
+            </Reveal>
+            <Reveal effect="fadeInUp">
+              <h4>Full-Stack Developer</h4>
+            </Reveal>
+            <Fade bottom cascade duration={1000}>
+              <SocialLinks>
+                {socialLinks &&
+                  socialLinks.map(item => {
+                    return (
+                      <li key={item.name}>
+                        <a href={item.url}>
+                          <FontAwesomeIcon icon={item.className} />
+                        </a>
+                      </li>
+                    );
+                  })}
+              </SocialLinks>
+            </Fade>
+          </BannerContent>
+        </Container>
+      </BannerArea>
+    </Home>
+  );
 }
+
+Header.propTypes = {
+  name: PropTypes.string,
+  socialLinks: PropTypes.array,
+  typedText: PropTypes.array,
+};
 
 export default Header;
