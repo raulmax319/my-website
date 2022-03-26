edadacd03db2095678608d797973ba9336475353
Raul Max <raulmax319@gmail.com>
2022-03-26 18:59:08 -0300

    feat: navbar: added navbar and scroll logic

---
+++ src/ui/pages/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './nav-bar.styles.scss';
+
+export const NavBar: React.FC = () => {
+  const [scrolled, setScrolled] = React.useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolled(true);
+      return;
+    }
+    setScrolled(false);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`${styles.nav} ${(scrolled && styles.sticky) || ''}`}>
+      <ul>
+        <li>
+          <a href="">HOME</a>
+        </li>
+        <li>
+          <a href="">ABOUT</a>
+        </li>
+        <li>
+          <a href="">CONTACT</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- src/ui/pages/home/home.tsx
+++ src/ui/pages/home/home.tsx
@@ -6,36 +6,44 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { NavBar } from '../components';
 import styles from './home.styles.scss';
 
 export const Home: React.FC = () => (
-  <div className={styles.home}>
-    <div className={styles.container}>
-      <h5>HELLO</h5>
-      <h2>I&#39;m Raul Max</h2>
-      <h4>Mobile Engineer</h4>
-      <ul className={styles.social}>
-        <li>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedinIn} />
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </li>
-      </ul>
+  <>
+    <div className={styles.home}>
+      <NavBar />
+      <div className={styles.container}>
+        <div>
+          <h5>HELLO</h5>
+          <h2>I&#39;m Raul Max</h2>
+          <h4>Mobile Engineer</h4>
+
+          <ul className={styles.social}>
+            <li>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
+    <div style={{ height: 100 }} />
+  </>
 );
