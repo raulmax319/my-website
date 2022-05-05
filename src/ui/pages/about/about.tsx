e5b8ef688d9f1d047e7da0f0b96fa115ca1a810a
Raul Max <raulmax319@gmail.com>
2022-05-05 14:56:20 -0300

    feat: about page

---
+++ src/ui/pages/about/about.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SectionTitle, Stack } from '../components';
+
+import styles from './about.styles.scss';
+
+export const About: React.FC = () => (
+  <section className={styles.about}>
+    <div className={styles.content}>
+      <SectionTitle>About me</SectionTitle>
+      <div className={styles.grid}>
+        <div className={styles.pictureContainer}>
+          <figure className={styles.circle}>
+            <img
+              src="https://avatars.githubusercontent.com/u/17752968?v=4"
+              alt="Me"
+            />
+          </figure>
+        </div>
+        <div className={styles.aboutContainer}>
+          <h3>Things about me</h3>
+          <p>
+            I am a Mobile Software Engineer with a background in business and
+            management. I have a passion for learning new things and building
+            applications that make people&#39;s lives better.
+          </p>
+          <p className={styles.blue}>Some of my skills include:</p>
+          <Stack />
+        </div>
+      </div>
+    </div>
+  </section>
+);
--- src/ui/pages/components/nav-bar/nav-bar.tsx
+++ src/ui/pages/components/nav-bar/nav-bar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styles from './nav-bar.styles.scss';
 
 export const NavBar: React.FC = () => {
